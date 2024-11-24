33ce07fcac353d96ce5127c1732e53c9ca4c5315
Zach Baker <zach@zachbaker.dev>
2024-11-24 13:46:53 -0500

    fix: update codedeploy

--- deploy/postinstall.sh
+++ deploy/postinstall.sh
@@ -2,3 +2,4 @@ cd /home/ubuntu/easyinstall || exit 1
 sudo n 20
 npm install
 npm run build
+sudo chmod -hR ubuntu ./
--- deploy/start.sh
+++
@@ -1,3 +0,0 @@
-cd /home/ubuntu/easyinstall || exit 1
-pm2 start ecosystem.config.js
-pm2 save
